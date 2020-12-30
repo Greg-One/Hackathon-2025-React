6f2ec75df10a3e001dc8e4ea3d7ee6d17241955c
Oleg <appphone100@gmail.com>
2020-12-30 12:36:33 +0300

    fix: scroll page

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import ReactDOM from 'react-dom';
 import './assets/vendor/normalize.css';
 import './pages/index.css';
 import App from './App';
+import ScrollToTop from './ScrollToTop';
+
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <ScrollToTop />
             <App/>
         </BrowserRouter>
     </React.StrictMode>,
